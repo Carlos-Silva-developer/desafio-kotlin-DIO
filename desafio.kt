ea639c899c9380e75f00857ac3c1debdd2a89ba1
Carlos Darci Melo da Silva <devcarlosdms@gmail.com>
2023-09-21 19:34:46 -0300

    Entrega do desafio kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+data class Usuario(val nome: String)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    @Throws
+    fun matricular(vararg usuarios: Usuario) {
+        try {
+            for(usuario in usuarios) {
+                inscritos.add(usuario) }
+            }
+        catch (e: Throwable) {
+            e.printStackTrace()
+        }
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val disciplinas = ArrayList<ConteudoEducacional>()
+    disciplinas.add(ConteudoEducacional("Introdução ao kotlin"))
+    disciplinas.add(ConteudoEducacional("kotlin Intermediário"))
+    val curso = Formacao("Curso basico de lotlin", Nivel.BASICO, disciplinas)
+
+
+    curso.run {
+        matricular(Usuario("cleitinho"), Usuario("Ronaldo"))
+    }
+
+    print(curso.inscritos)
+
+
 }
