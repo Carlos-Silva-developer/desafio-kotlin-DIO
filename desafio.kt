f6195ad57a3df9e7bfe65a51630d8d1e2a343e30
Carlos Darci Melo da Silva <devcarlosdms@gmail.com>
2023-09-21 22:07:42 -0300

    melhorando o código

--- desafio.kt
+++ desafio.kt
@@ -1,3 +1,4 @@
+
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 data class Usuario(val nome: String)
 data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
@@ -14,22 +15,27 @@ data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<Cont
         catch (e: Throwable) {
             e.printStackTrace()
         }
-
     }
+
+    fun listarInscritos() {
+        for (usuario in inscritos)
+            println(usuario.nome)
+    }
+
 }
 
 fun main() {
+
     val disciplinas = ArrayList<ConteudoEducacional>()
-    disciplinas.add(ConteudoEducacional("Introdução ao kotlin"))
-    disciplinas.add(ConteudoEducacional("kotlin Intermediário"))
+    disciplinas.run {
+        add(ConteudoEducacional("Introdução ao kotlin"))
+        add(ConteudoEducacional("kotlin Intermediário"))
+    }
+
     val curso = Formacao("Curso basico de lotlin", Nivel.BASICO, disciplinas)
-
-
     curso.run {
         matricular(Usuario("cleitinho"), Usuario("Ronaldo"))
     }
 
-    print(curso.inscritos)
+    curso.listarInscritos()
 
-
-}
